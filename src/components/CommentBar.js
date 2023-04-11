6848552932dcf617ed77e8598b3146923370ca51
Mabrur Herzaridano <mabrurherza@gmail.com>
2023-04-11 21:38:35 +0700

    fix error: @sanity/image hasnt been installed

--- src/components/CommentBar.js
+++ src/components/CommentBar.js
@@ -46,20 +46,16 @@ export default function CommentBar({ section }) {
   };
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(
-      q,
-      (querySnapshot) => {
-        setComment(
-          querySnapshot.docs.map((doc) => ({
-            ...doc.data(),
-            id: doc.id,
-            created: doc.data().created?.toDate().toLocaleDateString("en-GB"),
-          }))
-        );
-        setLoading(false);
-      },
-      [q]
-    );
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setComment(
+        querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+          created: doc.data().created?.toDate().toLocaleDateString("en-GB"),
+        }))
+      );
+      setLoading(false);
+    });
 
     return unsubscribe;
   }, []);
@@ -90,7 +86,7 @@ export default function CommentBar({ section }) {
         {loading ? (
           <div className="flex gap-2 items-center">
             <svg
-              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+              className="animate-spin ml-1 mr-3 h-4 w-4 text-white"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
